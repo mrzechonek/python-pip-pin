9cf417092e0fcb00d07f7f10c044a91826e7044c
Bartosz Flis <bartosz.flis@silvair.com>
2022-12-29 12:48:58 +0100

    Support defining dependencies in pyproject.toml
    Project dependencies in pyproject.toml config file can only be defined
    in [project.dependencies] and [project.optional-dependencies] tables.
    Since setuptools 61.0.0 they are mapped to attributes in the
    Distribution object (see
    https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html).
    
    This commit enables pinning dependencies in projects which use only
    pyproject.toml and a stub setup.py file.
    
    For an overview of gradual setup.py deprecation see
    https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html.

--- pip_pin/egg_info.py
+++ pip_pin/egg_info.py
@@ -1,13 +1,15 @@
+from pip_pin import util
+
+
 def tests_require(cmd, basename, filename):
     cmd.write_file(
-        "tests_require", filename, "\n".join(cmd.distribution.tests_require or [])
+        "tests_require", filename, "\n".join(util.get_tests_require(cmd.distribution))
     )
 
 
 def develop_requires(cmd, basename, filename):
-    try:
-        reqs = cmd.distribution.develop_requires or []
-    except AttributeError:
-        reqs = []
-
-    cmd.write_file("develop_requires", filename, "\n".join(reqs))
+    cmd.write_file(
+        "develop_requires",
+        filename,
+        "\n".join(util.get_develop_requires(cmd.distribution)),
+    )
